08c87dd7b638b188fc7113306dcec5d7ededed0a
irishandyesiam <nathan.irish@gmail.com>
2022-09-14 10:38:03 -0500

    Added Create Button to CreatePost component.

--- src/Components/CreatePost/CreatePost.jsx
+++ src/Components/CreatePost/CreatePost.jsx
@@ -7,12 +7,22 @@ const CreatePost = (props) => {
     const [name, setName] = useState('');
     const [post, setPost] = useState('');
 
+    function handleSubmit(event) {
+        event.preventDefault();
+        let newPost = {
+            name: name,
+            post: post,
+        };
+        console.log(newPost);
+    }
+
     return(
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>Name</label>
             <input type='name' value={name} onChange={(event) => setName(event.target.value)}/>
             <label>Post</label>
             <input type='post' value={post} onChange={(event) => setPost(event.target.value)}/>
+            <button type='submit'>Create</button>
         </form>
 
     );
